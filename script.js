19807ae842fc47c88bf45e98e45c3beb91be46f9
Roman <romanrokel@gmail.com>
2021-10-14 19:33:54 +0300

    All files upload. Everything needful exist!

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿let
+    str,
+    input = document.getElementById("input"),
+    output = document.getElementById("output");
+
+function Clear(){
+    input.value = "";
+    output.innerHTML = "";
+}
+
+function Run(){
+    output.innerHTML = "";
+    str = new String(input.value);
+    for(let i = 0; i < str.length; i+=2){
+        str = str.substr(0, i) + ' ' + str.substr(i, str.length);
+    }
+    output.innerHTML = str;
+}
